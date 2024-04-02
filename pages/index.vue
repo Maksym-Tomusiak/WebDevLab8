1ed8436de2ead90cfbe0f069a0e1831e6945c170
Maxym Tomusiak <maxtomusiak315@gmail.com>
2024-04-02 16:11:06 +0300

    fix: fix sort by columns, change index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <ListOfStudents/>
+  <div class="bg-emerald-400 text-white border-2 border-black rounded text-4xl w-8/12 text-center m-auto my-4 p-4">
+    <nuxt-link to="/listOfStudents">Vie list of students</nuxt-link>
+  </div>
 </template>
 <script setup lang="ts">
-
-import ListOfStudents from "~/pages/listOfStudents.vue";
 </script>
--- pages/listOfStudents.vue
+++ pages/listOfStudents.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
-import {ref, computed} from 'vue';
-
+useHead({
+  title: 'List of students',
+  meta: [
+    { name: 'description', content: 'My amazing site.' }
+  ],
+  bodyAttrs: {
+    class: 'test'
+  },
+  script: [ { innerHTML: 'console.log(\'Hello world\')' } ]
+})
 const columns = [
   {key: 'title', label: 'Title',sortable: true},
   {key: 'description', label: 'Description',sortable: true},
@@ -11,13 +19,111 @@ const columns = [
   {key: 'thumbnail', label: 'Thumbnail',sortable: true}
 ]
 
-const { data } = await useFetch<any>('https://dummyjson.com/products')
-const products = data.value.products
+const { data } = await useLazyFetch<any>('https://dummyjson.com/products')
+let products = data.value.products
 const q = ref('')
 const page = ref(1)
 const pageCount = 4
 const total = ref(products.length)
 
+/*
+//////////////////
+const sort = ref({ column: 'title', direction: 'asc' as const })
+const sortedRows = computed(() => {
+  const sortedProducts = [...products]
+  const { column, direction } = sort.value
+
+  if (column && direction) {
+    sortedProducts.sort((a, b) => {
+      const aValue = a[column]
+      const bValue = b[column]
+      if (aValue < bValue) return direction === 'asc' ? -1 : 1
+      if (aValue > bValue) return direction === 'asc' ? 1 : -1
+      return 0
+    })
+  }
+  return sortedProducts
+})
+
+// Compute filtered and paginated rows based on the search query, sorted data, and pagination settings
+const rows = computed(() => {
+  console.log(sortedRows)
+  let filteredProducts = [...sortedRows.value]
+
+  if (!q.value) {
+    total.value = products.length
+    return products.slice((page.value - 1) * pageCount, (page.value) * pageCount);
+  }
+  page.value = 1;
+  let result = products.filter((product: any) => {
+    return Object.values(product).some(value =>
+        String(value).toLowerCase().includes(q.value.toLowerCase())
+    );
+  })
+  total.value = result.length
+  filteredProducts = result.slice((page.value - 1) * pageCount, (page.value) * pageCount);
+
+  const startIndex = (page.value - 1) * pageCount
+  const endIndex = startIndex + pageCount
+
+  return filteredProducts.slice(startIndex, endIndex)
+})
+*/
+//////////////////
+
+const sort = ref({ column: 'title', direction: 'asc' as const })
+const sortedRows = computed(() => {
+  const sortedProducts = [...products]
+  const { column, direction } = sort.value
+
+  if (column && direction) {
+    sortedProducts.sort((a, b) => {
+      const aValue = a[column]
+      const bValue = b[column]
+      if (aValue < bValue) return direction === 'asc' ? -1 : 1
+      if (aValue > bValue) return direction === 'asc' ? 1 : -1
+      return 0
+    })
+  }
+  console.log(sortedProducts)
+  return sortedProducts
+});
+
+// Compute filtered and paginated rows based on the search query, sorted data, and pagination settings
+const rows = computed(() => {
+  let filteredProducts = [...sortedRows.value]
+
+  if (q.value) {
+    filteredProducts = filteredProducts.filter(product => {
+      return Object.values(product).some(value => {
+        return String(value).toLowerCase().includes(q.value.toLowerCase())
+      })
+    })
+  }
+
+  total.value = filteredProducts.length;
+  const startIndex = (page.value - 1) * pageCount
+  const endIndex = startIndex + pageCount
+
+  return filteredProducts.slice(startIndex, endIndex)
+})
+
+const filteredRows = computed(() => {
+  if (!q.value) {
+    total.value = products.value.length;
+    return products.value.slice((page.value - 1) * pageCount, (page.value) * pageCount);
+  }
+  page.value = 1;
+  let result = products.value.filter((product: any) => {
+    return Object.values(product).some(value =>
+        String(value).toLowerCase().includes(q.value.toLowerCase())
+    );
+  });
+  total.value = result.length;
+  return result.slice((page.value - 1) * pageCount, (page.value) * pageCount);
+});
+
+/*
 const filteredRows = computed(() => {
   if (!q.value) {
     total.value = products.length
@@ -32,7 +138,9 @@ const filteredRows = computed(() => {
   total.value = result.length
   return result.slice((page.value - 1) * pageCount, (page.value) * pageCount);
 })
+*/
 const selected = ref([])
+
 </script>
 
 <template>
@@ -40,7 +148,7 @@ const selected = ref([])
     <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
       <UInput v-model="q" placeholder="Filter products..."/>
     </div>
-    <UTable class="w-full" v-model="selected" :ui="{ td: { base: 'mx-auto max-w-[0] truncate' } }" :columns="columns" :rows="filteredRows">
+    <UTable class="w-full" v-model:sort="sort" sort-mode="manual" v-model="selected" :ui="{ td: { base: 'mx-auto max-w-[0] truncate' } }" :columns="columns" :rows="rows">
       <template #thumbnail-data="{ row }">
         <img class="w-[100px] h-[100px]" :src="row.thumbnail" alt="Thumbnail" />
       </template>
